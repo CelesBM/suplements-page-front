9928493b52d651d31a0a9ab25500928963476d24
unknown <celesbmontero@hotmail.com>
2023-11-19 19:04:36 -0300

    router 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import "./App.css";
-//import Routes from "./routes/Routes";
 import { GlobalStyles } from "./styles/GlobalStyles";
-//import Home from "./pages/Home";
-//import About from "./pages/About/About";
-//import Products from "./pages/Products/Products.jsx";
-//import Contact from "./pages/Contact/Contact.jsx";
-//import Header from "./components/Header/Header";
 import { ContextProvider } from "./Context/Context";
-//import Hero from "./components/Hero/Hero";
-//import Training from "./components/Training";
-//import Footer from "./components/Footer";
 import Router from "./Router/Router";
 
 function App() {
