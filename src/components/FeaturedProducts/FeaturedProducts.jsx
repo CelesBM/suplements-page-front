c275ed04b2e845c55992c6977bdac7c427e4eff3
unknown <celesbmontero@hotmail.com>
2023-12-18 09:44:31 -0300

    featured products modified

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import {
   ContainerProductsStyled,
   ProductsStyled,
-  ProductsStyled,
+  ProductStyled,
   InfoProductStyled,
   ButtonProductStyled,
 } from "./FeaturedProductsStyles";
